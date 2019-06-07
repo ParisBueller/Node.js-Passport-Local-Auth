cd644721242dcf6cb4170396125b6823b030d085
Paris Ward <wardparis.pw@gmail.com>
2019-06-07 09:40:57 -0700

    hash password, save user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -36,7 +40,40 @@ router.post('/register', (req, res) => {
             password2
         });
     } else {
-        res.send('pass');
+        //validation passed
+        User.findOne({ email: email})
+            .then(user => {
+                if(user) {
+                    errors.push({ msg: 'Email is already registered'})
+                    res.render('register', {
+                        errors,
+                        name,
+                        email, 
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    //Hash password
+                    bcrypt.genSalt(10, (err, salt) => 
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                           if (err) throw err;
+                           //set our password to the hashed(encrypted) password
+                           newUser.password = hash;
+                           newUser.save()
+                            .then(user => {
+                                req.flash('success_msg', 'You are now registered and can log in!');
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => console.log(err));
+                    }))
+                }
+            });
     }
 
 });
