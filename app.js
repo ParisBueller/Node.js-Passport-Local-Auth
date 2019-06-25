8cfdbc96a37fa6cfe35a173b5ff62482bf179249
Paris Ward <wardparis.pw@gmail.com>
2019-06-25 09:24:26 -0700

    local strategy, auth guard

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+require('./config/passport')(passport);
 
 //DB config
 const db = require('./config/keys').MongoURI;
@@ -24,7 +27,10 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-}))
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connect flash
 app.use(flash());
@@ -33,6 +39,7 @@ app.use(flash());
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 
